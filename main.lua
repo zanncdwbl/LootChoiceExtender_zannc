b0c9a4b7b8141e98bdfd7f789d53b5a96b97d48f
x00179520 <xspelz@gmail.com>
2024-05-14 19:28:47 +0100

    refactor of how it works...

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 if not zanncModMain.Config.Enabled then return end
 
 local dataconfigs = {
+    --Max of like 6, before it starts breaking
     ExtraChoices = 2
 }
 
@@ -33,35 +34,60 @@ ModUtil.Path.Override("CalcNumLootChoices", function( )
 	return numChoices
 end, zanncModMain)
 
-ModUtil.Path.Context.Wrap("CreateUpgradeChoiceButton", function ( screen, lootData, itemIndex, itemData )
-    local purchaseButton = ShallowCopyTable( screen.PurchaseButton )
+ModUtil.Path.Context.Wrap("CreateUpgradeChoiceButton", function ( screen )
+    -- local purchaseButton = ShallowCopyTable( screen.PurchaseButton )
+	-- local highlight = ShallowCopyTable( screen.Highlight )
+
     local data = { }
-    if purchaseButton.Name == "BoonSlotBase" and purchaseButton.Group == "Combat_Menu" then -- hopefully to stop breaking the codex/arachne/echo etc
-        local locals = ModUtil.Locals.Stacked( )
-        data.upgrade = locals.upgradeData
+    local active = false
 
-        data.squash = 3/(3+dataconfigs.ExtraChoices)
-    
-        screen.ButtonSpacingY = ScreenData.UpgradeChoice.ButtonSpacingY * (data.squash ^ 1) -- Spacing between buttons automatically to scale - Lower 0.8 for more space
-        -- ModUtil.Hades.PrintStack(screen.ButtonSpacingY)
+    ModUtil.Path.Wrap("CreateScreenComponent", function ( base, args )
+        if not active and args.Group == "Combat_Menu" then
+            active = true
+            local local_hades = ModUtil.Locals.Stacked( )
 
-        screen.PurchaseButton.Y = itemLocationY -- dunno if i need this but will keep so location stops crying
-        screen.PurchaseButton.Scale = 1.0 * (data.squash ^ 0.8) -- Scaling the buttons, unsure how to get it to scale (specifically the X scaling) nicely like in hades 1 - increase 0.8 for harsher scaling
-        screen.Highlight.Scale = screen.PurchaseButton.Scale -- same as purchaseButton scaling
-    end
+            data.upgrade = local_hades.upgradeData
+            data.squash = 3/(3+dataconfigs.ExtraChoices)
+
+            if args.Name == "BoonSlotBase" then
+                local_hades.itemLocationY = local_hades.itemLocationY + 100 * (data.squash - 1)
+                args.Y = local_hades.itemLocationY
+                args.Scale = 1.0 * (data.squash ^ 0.4)
+                screen.Highlight.Scale = args.Scale -- Can't find a good way to do this
+            end
+            -- if args.Name == "BlankObstacle" then
+            --     local_hades.itemLocationY = local_hades.itemLocationY + 100 * (data.squash - 1)
+            --     args.Y = local_hades.itemLocationY
+            --     args.Scale = 1.0 * (data.squash ^ 0.4)
+            -- end
+        end
+        local component = base( args ) 
+		return component
+    end)
+    -- if purchaseButton.Group == "Combat_Menu" then -- hopefully to stop breaking the codex/arachne/echo etc
+        -- if purchaseButton.Name == "BoonSlotBase" then
+        --     screen.ButtonSpacingY = ScreenData.UpgradeChoice.ButtonSpacingY * (data.squash ^ (1/3)) -- Spacing between buttons automatically to scale - Lower 0.8 for more space
+        --     -- ModUtil.Path.Wrap("CreateScreenComponent", function (base, args)
+                
+        --     -- end)
+
+        --     -- screen.PurchaseButton.Y = itemLocationY -- dunno if i need this but will keep so location stops crying
+        --     screen.PurchaseButton.Scale = 1.0 * (data.squash ^ (1/3)) -- Scaling the buttons, unsure how to get it to scale (specifically the X scaling) nicely like in hades 1 - increase 0.8 for harsher scaling
+        --     screen.Highlight.Scale = screen.PurchaseButton.Scale -- same as purchaseButton scaling
+        -- end
+    -- end
 
     ModUtil.Path.Wrap("CreateTextBox", function( base, args )
-        if args.OffsetY and args.OffsetX then 
-            args.OffsetY = args.OffsetY * (data.squash ^ 0.55)
-            args.OffsetX = args.OffsetX * (data.squash ^ 0.6)
+        if args.OffsetY then
+            args.OffsetY = args.OffsetY * data.squash
         end
-
-        if args.FontSize then 
-            args.FontSize = args.FontSize * (data.squash ^ 0.5)
+        if args.OffsetX then
+            args.OffsetX = args.OffsetX * (data.squash ^ (1/3))
         end
-        -- if data.upgrade and args.Text == data.upgrade.CustomRarityName then 
-        --     ModUtil.Locals.Stacked( ).lineSpacing = 8*data.squashY
-        -- end
+        if args.FontSize then args.FontSize = args.FontSize * (data.squash ^ (1/3)) end
+        if data.upgrade and args.Text == data.upgrade.CustomRarityName then 
+            ModUtil.Locals.Stacked( ).lineSpacing = 8*data.squash
+        end
         return base( args )
     end)
 end)
@@ -69,7 +95,7 @@ end)
 ModUtil.Path.Override("DestroyBoonLootButtons", function( screen, lootData )
 	local components = screen.Components
 	local toDestroy = {}
-	for index = 1, ScreenData.UpgradeChoice.MaxChoices + dataconfigs.ExtraChoices do -- indexing to new max limit
+	for index = 1, GetTotalLootChoices() do -- indexing to new max limit
 		local destroyIndexes = {
 		"PurchaseButton"..index,
 		"PurchaseButton"..index.. "Lock",
